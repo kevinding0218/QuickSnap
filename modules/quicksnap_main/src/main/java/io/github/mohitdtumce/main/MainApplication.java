322a7fa16d39fcf2fc6e6d7e0d6ea74fb21f3caa
mohitsharma-r <mohitsharma.r@flipkart.com>
2019-08-25 02:16:50 +0530

    Setting up basic project structure

---
+++ modules/quicksnap_main/src/main/java/io/github/mohitdtumce/main/MainApplication.java
@@ -0,0 +1,11 @@
+package io.github.mohitdtumce.main;
+
+import io.github.mohitdtumce.profile.*;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Profile profile = new Profile();
+        System.out.println(profile.testProfile());
+    }
+}
